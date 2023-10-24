a0adf0eacb0701afecf3d02e1a28a2a1b63d28f8
97gamjak <97gamjak@gmail.com>
2023-10-25 00:47:01 +0200

    read_frame is not implemented for strings and not files

--- PQAnalysis/core/frame.py
+++ PQAnalysis/core/frame.py
@@ -1,38 +1,43 @@
 import numpy as np
+from .cell import Cell
 
-def read_frame(filename):
+
+def read_frame(frame_string):
     '''
     Read a frame from a file.
     '''
-    file = open(filename, 'r')
-    splitted_line = file.readline().split()
-    if len(splitted_line) == 4:
-        n_atoms, a, b, c = int(splitted_line[0]), map(float, splitted_line[1:4])
-        cell = cell.Cell(a, b, c)
-    elif len(splitted_line) == 7:
-        n_atoms, a, b, c, alpha, beta, gamma = int(splitted_line[0]), map(float, splitted_line[1:7])
-        cell = cell.Cell(a, b, c, alpha, beta, gamma)
-    elif len(splitted_line) == 1:
-        n_atoms= int(splitted_line[0])
+
+    splitted_frame_string = frame_string.split('\n')
+    header_line = splitted_frame_string[0].split()
+    if len(header_line) == 4:
+        n_atoms = int(header_line[0])
+        a, b, c = map(float, header_line[1:4])
+        cell = Cell(a, b, c)
+    elif len(header_line) == 7:
+        n_atoms = int(header_line[0])
+        a, b, c, alpha, beta, gamma = map(float, header_line[1:7])
+        cell = Cell(a, b, c, alpha, beta, gamma)
+    elif len(header_line) == 1:
+        n_atoms = int(header_line[0])
         cell = None
     else:
         raise ValueError('Invalid file format in header line of Frame.')
-    
-    file.readline() # Skip the second line
 
     xyz = np.zeros((n_atoms, 3))
     atoms = []
     for i in range(n_atoms):
-        line = file.readline()
+        line = splitted_frame_string[2+i]
         xyz[i] = np.array([float(x) for x in line.split()[1:4]])
         atoms.append(line.split()[0])
 
     return Frame(n_atoms, xyz, atoms, cell)
 
+
 class Frame:
     '''
     A class to represent a frame of a trajectory.
     '''
+
     def __init__(self, n_atoms, xyz, atoms, cell=None):
         self.n_atoms = n_atoms
         self.xyz = xyz
