ea648784e7439da3f6ffa79f017c5430d6e3b44b
Jakob Gamper <97gamjak@gmail.com>
2023-10-28 10:52:22 +0200

    documentation and cleaning

---
+++ PQAnalysis/utils/exceptions.py
@@ -0,0 +1,10 @@
+class ElementNotFoundError(Exception):
+    """
+    Exception raised if the given element id is not valid
+    """
+
+    def __init__(self, id):
+        self.id = id
+        self.message = f"""Element with id {
+            self.id} is not a valid element identifier."""
+        super().__init__(self.message)
--- tests/atom/test_element.py
+++ tests/atom/test_element.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 
 from PQAnalysis.atom.element import Element
+from PQAnalysis.utils.exceptions import ElementNotFoundError
 
 
 def test__init__():
@@ -15,10 +16,20 @@ def test__init__():
     assert element.atomic_number == 2
     assert np.isclose(element.mass, 4.002602)
 
-    with pytest.raises(ValueError) as exception:
+    with pytest.raises(TypeError) as exception:
         Element(1.2)
     assert str(
-        exception.value) == "Invalid element id - must be either atomic number or element name."
+        exception.value) == "Invalid type for element id - must be either int (atomic number) or str (element name)."
+
+    with pytest.raises(ElementNotFoundError) as exception:
+        Element(-1)
+    assert str(
+        exception.value) == "Element with id -1 is not a valid element identifier."
+
+    with pytest.raises(ElementNotFoundError) as exception:
+        Element("CH")
+    assert str(
+        exception.value) == "Element with id CH is not a valid element identifier."
 
 
 def test__eq__():
