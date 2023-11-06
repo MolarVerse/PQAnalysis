df6d81593de3bc867acfa314c712fb4131d8e96e
97gamjak <97gamjak@gmail.com>
2023-11-06 10:23:56 +0100

    full API and cli tested - only one topology line missing because not implemented yet

--- PQAnalysis/io/energyFileReader.py
+++ PQAnalysis/io/energyFileReader.py
@@ -82,7 +82,7 @@ class EnergyFileReader(BaseReader):
 
         if format not in self.formats:
             raise ValueError(
-                f"The format {format} is not supported. Supported formats are {self.formats}.")
+                f"Format {format} is not supported. Supported formats are {self.formats}.")
 
         self.format = format
 
