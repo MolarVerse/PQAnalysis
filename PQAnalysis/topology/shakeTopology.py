612239aebdc621f77f8041f37ae3f4180cf8032e
97gamjak <97gamjak@gmail.com>
2023-11-14 00:52:48 +0100

    generate topology function implementd - not tested yet

--- PQAnalysis/topology/shakeTopology.py
+++ PQAnalysis/topology/shakeTopology.py
@@ -1,59 +1,64 @@
+import numpy as np
+
 from multimethod import multimethod
+from beartype.typing import Tuple
 
 from ..core.atom import Atom
 from ..traj.trajectory import Trajectory
-from ..types import Np1DIntArray
+from ..types import Np1DIntArray, Np1DNumberArray
 
 
-class ShakeTopology:
-    def __init__(self, atoms: list[Atom] | None = None) -> None:
-        if atoms is None:
-            atoms = []
-
-        self.atoms = atoms
+class ShakeTopologyGenerator:
+    @multimethod
+    def __init__(self) -> None:
+        self._atoms = None
+        self._use_full_atom_info = False
 
     @multimethod
-    def generate_topology(self, trajectory: Trajectory, type: Atom) -> None:
-        """
-        Generates the topology.
-        """
-
-        for frame in trajectory:
-            pass
-
-        raise NotImplementedError
-
-    @multimethod
-    def generate_topology(self, trajectory: Trajectory, indices: Np1DIntArray) -> None:
-        """
-        Generates the topology.
-        """
-        raise NotImplementedError
-
-    ##############
-    # Properties #
-    ##############
-
-    @property
-    def atoms(self) -> list[Atom]:
-        """
-        The atoms in the system.
-
-        Returns
-        -------
-        list[Atom]
-            The atoms in the system.
-        """
-        return self._atoms
-
-    @atoms.setter
-    def atoms(self, atoms: list[Atom]) -> None:
-        """
-        The atoms in the system.
-
-        Parameters
-        ----------
-        atoms : list[Atom]
-            The atoms in the system.
-        """
+    def __init__(self, atoms: list[Atom], use_full_atom_info: bool = False) -> None:
         self._atoms = atoms
+        self._use_full_atom_info = use_full_atom_info
+
+    @multimethod
+    def __init__(self, atoms: list[str]):
+        self._atoms = [Atom(name) for name in atoms]
+        self._use_full_atom_info = False
+
+    @multimethod
+    def __init__(self, indices: Np1DIntArray):
+        self._atoms = indices
+        self._use_full_atom_info = False
+
+    @multimethod
+    def generate_topology(self, trajectory: Trajectory) -> Tuple[Np1DIntArray, Np1DIntArray, Np1DNumberArray]:
+        """
+        Generates the topology.
+        """
+
+        start_frame = trajectory[0]
+        target_indices, distances = start_frame.nearest_neighbours(
+            n=1, atoms=self.atoms, use_full_atom_info=self._use_full_atom_info)
+
+        target_indices = target_indices.flatten()
+        distances = distances.flatten()
+
+        indices = start_frame.indices_from_atoms(
+            self.atoms, use_full_atom_info=self._use_full_atom_info)
+
+        distances = [distances]
+
+        for frame in trajectory[1:]:
+            pos = frame.pos[indices]
+            target_pos = frame.pos[target_indices]
+
+            delta_pos = pos - target_pos
+
+            delta_pos = frame.cell.image(delta_pos)
+
+            distances.append(np.linalg.norm(delta_pos, axis=1))
+
+            frame_distances = np.linalg.norm(delta_pos, axis=1)
+
+            distances.append(frame_distances)
+
+        return indices, target_indices, np.mean(np.array(distances), axis=1)
