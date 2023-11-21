38e883eb74108a70ec4788ce40895b70b136d2b9
Jakob Gamper <97gamjak@gmail.com>
2023-11-21 15:21:11 +0100

    added restart file writer

--- PQAnalysis/topology/topology.py
+++ PQAnalysis/topology/topology.py
@@ -2,10 +2,14 @@ from ..types import Np1DIntArray
 
 
 class Topology:
+
+    def __init__(self):
+        self.mol_types = None
+
     @property
     def mol_types(self):
         return self._mol_types
 
     @mol_types.setter
-    def mol_types(self, value: Np1DIntArray):
+    def mol_types(self, value: Np1DIntArray | None):
         self._mol_types = value
