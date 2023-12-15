c501d29af6f3dba3988f3020a7f9dd165871b211
97gamjak <97gamjak@gmail.com>
2023-12-15 09:08:10 +0100

    cleaned up PIMD_QMCF_InputFileReader

--- PQAnalysis/io/inputFileReader/PIMD_QMCF/PIMD_QMCF_inputFileReader.py
+++ PQAnalysis/io/inputFileReader/PIMD_QMCF/PIMD_QMCF_inputFileReader.py
@@ -1,94 +1,195 @@
+"""
+A module containing the PIMD_QMCF_InputFileReader class.
+
+Classes
+-------
+PIMD_QMCF_InputFileReader
+    Reads a PIMD_QMCF input file and parses it.
+    
+Functions
+---------
+_increase_digit_string
+    increases a string containing only digits by one
+_get_digit_string_from_filename
+    extracts a string containing only digits from a filename
+"""
+
+import re
+
 from ..formats import InputFileFormat
 from ..inputFileParser import InputFileParser
 from .output_files import _OutputFileMixin
 
 
-def _continue_digit_string(digit_string: str) -> str:
-    n_length = len(digit_string)
-    leading_zeros = 0
+def _increase_digit_string(digit_string: str) -> str:
+    """
+    increases a string containing only digits by one
 
-    for char in digit_string:
-        if char != "0":
-            break
-        else:
-            leading_zeros += 1
+    If the string contains leading zeros, they are preserved.
+    The total length of the string is preserved, if there are leading zeros.
 
-    digit_string = str(int(digit_string[leading_zeros:]) + 1)
+    For example:
+    "0001" -> "0002"
+    "001" -> "002"
+    "099" -> "100"
+    "999" -> "1000"
 
-    delta_n = n_length - len(digit_string)
-    for _ in range(delta_n):
-        digit_string = "0" + digit_string
+    Parameters
+    ----------
+    digit_string : str
+        string containing only digits to be increased
 
-    return digit_string
+    Returns
+    -------
+    str
+        increased string by one
+
+    Raises
+    ------
+    ValueError
+        if digit_string contains non-digit characters
+    """
+
+    if not all([char.isdigit() for char in digit_string]):
+        raise ValueError(
+            f"digit_string {digit_string} contains non-digit characters.")
+
+    string_without_leading_zeros = digit_string.lstrip("0")
+
+    if string_without_leading_zeros == "":
+        string_without_leading_zeros = "0"
+
+    string_without_leading_zeros = str(int(string_without_leading_zeros) + 1)
+
+    return "0" * (len(digit_string) - len(string_without_leading_zeros)) + string_without_leading_zeros
 
 
-def _get_n_from_filename(filename: str) -> str:
-    filename_splitted = filename.split(".", 1)[0]
+def _get_digit_string_from_filename(filename: str) -> str:
+    """
+    extracts a string containing only digits from a filename
 
-    counter = -1
-    digits = []
-    while filename_splitted[counter].isdigit():
-        digits = [filename_splitted[counter]] + digits
-        counter -= 1
+    The string has to be of the form "...<number>.<extension>".
+    The string is extracted from the number part of the filename.
 
-    if len(digits) == 0:
+    For example:
+    "filename_0001.extension" -> "0001"
+    "filename_001.extension" -> "001"
+    "filename_099.extension" -> "099"
+    "filename_100.extension" -> "100"
+    "filename.extension" -> ValueError
+
+    Parameters
+    ----------
+    filename : str
+        filename to be parsed
+
+    Returns
+    -------
+    str
+        string containing only digits
+
+    Raises
+    ------
+    ValueError
+        if filename does not contain a number to be parsed
+    """
+
+    regex = re.search(r"\d+.", filename)
+
+    if regex is None:
         raise ValueError(
             f"Filename {filename} does not contain a number to be continued from. It has to be of the form \"...<number>.<extension>\".")
 
-    n = "".join(digits)
-
-    return n
+    return regex.group(0)[:-1]
 
 
 class PIMD_QMCF_InputFileReader(_OutputFileMixin):
-    def __init__(self, filename):
+    """
+    Reads a PIMD_QMCF input file and parses it.
+
+    Parameters
+    ----------
+    _OutputFileMixin : class
+        mixin class containing all output and start file keys
+    """
+
+    def __init__(self, filename: str):
+        """
+        Initialize the PIMD_QMCF_InputFileReader class.
+
+        self.format is set to InputFileFormat.PIMD_QMCF.
+        A parser is created using the InputFileParser class.
+
+        Parameters
+        ----------
+        filename : str
+            filename of the input file
+        """
+
         self.format = InputFileFormat.PIMD_QMCF
         self.filename = filename
         self.parser = InputFileParser(self.filename, self.format)
 
     def read(self):
+        """
+        Reads the input file and parses it.
+
+        The dictionary is set to the dictionary returned by the parser.
+        The raw_input_file is set to the raw_input_file returned by the parser.
+        """
+
         self.dictionary = self.parser.parse()
         self.raw_input_file = self.parser.raw_input_file
 
     def continue_input_file(self, n: int):
-        self.input_file_n = _get_n_from_filename(self.filename)
-        self.old_n = self._parse_old_n()
+        """
+        Creates n new input files by increasing the number in the filename by one.
+        All other numbers in the start- and output-files within the input file are increased by one as well.
+
+        Parameters
+        ----------
+        n : int
+            number of new input files to be created
+
+        Raises
+        ------
+        ValueError
+            if the n parsed from the output files defined in the input file does not match the n parsed from the input file name
+        ValueError
+            if the n parsed from the start file does not match the n parsed from the output files
+        """
+        self.input_file_n = _get_digit_string_from_filename(self.filename)
+        self.start_n = self._parse_start_n()
         self.actual_n = self._parse_actual_n()
 
+        # check if n from input file name matches n from output files
         if int(self.actual_n) != int(self.input_file_n):
             raise ValueError(
                 f"Actual n ({self.actual_n}) and input file n ({self.input_file_n}) do not match.")
 
-        if int(self.old_n) != int(self.actual_n) - 1:
+        # check if n from start file matches n from output files
+        if int(self.start_n) != int(self.actual_n) - 1:
             raise ValueError(
-                f"Old n ({self.old_n}) has to be one less than actual n ({self.actual_n}).")
-
-        if int(self.input_file_n) >= n:
-            raise ValueError(
-                f"Input file n ({self.input_file_n}) has to be less than n ({n}).")
+                f"Old n ({self.start_n}) has to be one less than actual n ({self.actual_n}).")
 
         old_input_file_n = self.input_file_n
-        old_old_n = self.old_n
+        old_start_n = self.start_n
         old_actual_n = self.actual_n
 
-        for _ in range(int(self.actual_n), n):
-            new_input_file_n = _continue_digit_string(old_input_file_n)
-            new_old_n = _continue_digit_string(old_old_n)
-            new_actual_n = _continue_digit_string(old_actual_n)
+        for _ in range(n):
+            new_input_file_n = _increase_digit_string(old_input_file_n)
+            new_start_n = _increase_digit_string(old_start_n)
+            new_actual_n = _increase_digit_string(old_actual_n)
 
-            new_raw_input_file = self.raw_input_file
-
-            new_filename = self.filename.replace(
-                self.input_file_n, new_input_file_n)
-            file = open(new_filename, "w")
-
+            # replace digit strings in start files
             for key in self.start_file_keys:
                 if key in self.dictionary.keys():
                     new_filename = self.dictionary[key][0].replace(
-                        self.old_n, new_old_n)
-                    new_raw_input_file = new_raw_input_file.replace(
+                        self.start_n, new_start_n)
+                    new_raw_input_file = self.raw_input_file.replace(
                         self.dictionary[key][0], new_filename)
 
+            # replace digit strings in output files
             for key in self.output_file_keys:
                 if key in self.dictionary.keys():
                     new_filename = self.dictionary[key][0].replace(
@@ -96,40 +197,76 @@ class PIMD_QMCF_InputFileReader(_OutputFileMixin):
                     new_raw_input_file = new_raw_input_file.replace(
                         self.dictionary[key][0], new_filename)
 
+            # create new input file and write new_raw_input_file to it
+            new_filename = self.filename.replace(
+                self.input_file_n, new_input_file_n)
+            file = open(new_filename, "w")
             file.write(new_raw_input_file)
             file.close()
 
             old_input_file_n = new_input_file_n
-            old_old_n = new_old_n
+            old_start_n = new_start_n
             old_actual_n = new_actual_n
 
-    def _parse_old_n(self) -> str:
-        old_n = _get_n_from_filename(self.start_file)
+    def _parse_start_n(self) -> str:
+        """
+        Parses the n from the start file.
+
+        If the rpmd_start_file is defined, the n from the start_file and the rpmd_start_file are compared.
+        If they do not match, a ValueError is raised.
+
+        Returns
+        -------
+        str
+            n parsed from the start file as a digit string
+
+        Raises
+        ------
+        ValueError
+            if the n from the start file and the rpmd_start_file do not match
+        """
+        n = _get_digit_string_from_filename(self.start_file)
 
         if self.is_rpmd_start_file_defined:
-            _old_n = _get_n_from_filename(self.rpmd_start_file)
+            _n = _get_digit_string_from_filename(self.rpmd_start_file)
 
-            if _old_n != old_n:
+            if _n != n:
                 raise ValueError(
-                    f"Old n from start_file ({self.old_n}) and rpmd_start_file ({old_n}) do not match.")
+                    f"N from start_file ({n}) and rpmd_start_file ({_n}) do not match.")
 
-        return old_n
+        return n
 
     def _parse_actual_n(self) -> str:
-        actual_n = None
+        """
+        Parses the n from the output files.
+
+        Returns
+        -------
+        str
+            n parsed from the output files as a digit string
+
+        Raises
+        ------
+        ValueError
+            if no output file is defined
+        ValueError
+            if the n parsed from the output files is not consistent
+        """
+        n = None
 
         for key in self.output_file_keys:
             if key in self.dictionary.keys():
-                _actual_n = _get_n_from_filename(self.dictionary[key][0])
+                _n = _get_digit_string_from_filename(
+                    self.dictionary[key][0])
 
-                if _actual_n != actual_n and actual_n is not None:
+                if _n != n and n is not None:
                     raise ValueError(
                         f"Actual n in output files is not consistent.")
 
-                actual_n = _actual_n
+                n = _n
 
-        if actual_n is None:
+        if n is None:
             raise ValueError(
                 f"No output file found to determine actual n.")
 
-        return actual_n
+        return n
