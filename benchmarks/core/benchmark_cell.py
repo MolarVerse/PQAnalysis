075f8b38b016fcb51e127d5733941ab01761a884
Jakob Gamper <97gamjak@gmail.com>
2023-12-17 23:31:18 +0100

    added benchmark for image cell

---
+++ benchmarks/core/benchmark_cell.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from PQAnalysis.core.cell import Cell
+
+
+class BenchmarkCell:
+    def benchmark_image_orthorhombic(self, benchmark):
+        cell = Cell(
+            10, 10, 10, 90, 90, 90
+        )
+        pos = np.array([5, 5, 5])
+        benchmark(cell.image, pos)
+
+    def benchmark_image_triclinic(self, benchmark):
+        cell = Cell(
+            10, 10, 10, 90.00000000001, 90.00000000001, 90.00000000001
+        )
+        pos = np.array([5, 5, 5])
+        benchmark(cell.image, pos)
