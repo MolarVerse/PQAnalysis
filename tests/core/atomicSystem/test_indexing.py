cb36448280b227237fb9dae3d3da6d5de2bb32a7
97gamjak <97gamjak@gmail.com>
2023-12-21 00:22:59 +0100

    added warning if selection of system is empty

--- tests/core/atomicSystem/test_indexing.py
+++ tests/core/atomicSystem/test_indexing.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pytest
 
 from PQAnalysis.core import Atom, AtomicSystem
+from PQAnalysis.core.exceptions import AtomicSystemEmptySelectionWarning as EmptySelectionWarning
 
 
 class Test_IndexingMixin:
@@ -19,5 +21,37 @@ class Test_IndexingMixin:
 
     def test__indices_by_element_types(self):
         indices = self.system._indices_by_element_types([Atom(1), Atom(8)])
+
+        assert np.allclose(indices, [0, 1, 2, 3])
+
+    def test_indices_from_atoms(self):
+        indices = self.system.indices_from_atoms(['H', 'O'])
+
+        assert np.allclose(indices, [0, 1])
+
+        with pytest.warns(EmptySelectionWarning):
+            self.system.indices_from_atoms(['C'])
+
+        indices = self.system.indices_from_atoms(
+            [self.atoms[0]], use_full_atom_info=True)
+
+        assert np.allclose(indices, [0])
+
+        indices = self.system.indices_from_atoms(
+            [self.atoms[0]])
+
+        assert np.allclose(indices, [0, 2])
+
+        with pytest.warns(EmptySelectionWarning):
+            self.system.indices_from_atoms([Atom('C')])
+
+        indices = self.system.indices_from_atoms([Atom(1), Atom(8)])
+
+        assert np.allclose(indices, [0, 1, 2, 3])
+
+        with pytest.warns(EmptySelectionWarning):
+            self.system.indices_from_atoms([Atom(6)])
+
+        indices = self.system.indices_from_atoms(None)
 
         assert np.allclose(indices, [0, 1, 2, 3])
