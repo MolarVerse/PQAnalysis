25340ecaf7fd06b1ed77709fde093b1030624d12
97gamjak <97gamjak@gmail.com>
2024-01-02 13:11:05 +0100

    added docstrings to utils package

--- PQAnalysis/utils/__init__.py
+++ PQAnalysis/utils/__init__.py
@@ -1,2 +1,20 @@
+"""
+A package containing classes and functions with common use.
+
+The utils package contains the following submodules:
+        
+            - common
+            - decorators
+            
+The utils package contains the following functions:
+            
+            - print_header
+            
+The utils package contains the following decorators:
+            
+            - count_decorator
+            - instance_function_count_decorator
+"""
+
 from .common import print_header
 from .decorators import count_decorator, instance_function_count_decorator
