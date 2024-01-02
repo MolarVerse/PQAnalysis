b0147a892b7893564aeb706a3d0b5768447a8f28
Jakob Gamper <97gamjak@gmail.com>
2024-01-02 21:44:30 +0100

    refactored grammar for selections

--- PQAnalysis/topology/selection.py
+++ PQAnalysis/topology/selection.py
@@ -7,6 +7,10 @@ Classes
 -------
 Selection
     A class for representing a selection.
+SelectionTransformer
+    A class for transforming a Lark parse tree.
+CrudeSelectionVisitor
+    A super class for visiting a Lark parse tree.
 SelectionVisitor
     A class for visiting a Lark parse tree and returning the indices of parsed selection.
     
@@ -26,14 +30,14 @@ from __future__ import annotations
 
 import numpy as np
 
-from lark import Visitor, Tree, Lark
+from lark import Visitor, Tree, Lark, Transformer, Token
 from beartype.typing import List
 from multimethod import overload
 
 from .topology import Topology
 from .. import __base_path__
 from ..types import Np1DIntArray
-from ..core import Atom, Atoms
+from ..core import Atom, Atoms, Element
 
 """
 A type hint for a selection compatible object.
@@ -200,12 +204,223 @@ def _selection(string: str, topology: Topology, use_full_atom_info: bool) -> Np1
                        propagate_positions=True)
 
     tree = parser.parse(string)
+    transformed_tree = SelectionTransformer(visit_tokens=True).transform(tree)
     visitor = SelectionVisitor(topology, use_full_atom_info)
 
-    return np.sort(visitor.visit(tree))
+    return np.sort(visitor.visit(transformed_tree))
 
 
-class SelectionVisitor(Visitor):
+class SelectionTransformer(Transformer):
+    """
+    A class for transforming a Lark parse tree.
+
+    Parameters
+    ----------
+    Transformer : Transformer
+        The type of the Transformer class to inherit from.
+    """
+
+    def __init__(self, visit_tokens=False):
+        """
+        Initializes the SelectionTransformer with the given parameters.
+
+        Parameters
+        ----------
+        visit_tokens : bool, optional
+            Whether to visit the tokens, by default False
+        """
+        self.__visit_tokens__ = visit_tokens
+        super().__init__(visit_tokens=visit_tokens)
+
+    def word(self, items: Token) -> str:
+        """
+        Returns the word of the given token.
+
+        Parameters
+        ----------
+        items : Token
+            The token to get the word of.
+
+        Returns
+        -------
+        str
+            The word of the given token.
+        """
+        return "".join(items)
+
+    def letters(self, items: Token) -> str:
+        """
+        Returns the letters of the given token.
+
+        Parameters
+        ----------
+        items : Token
+            The token to get the letters of.
+
+        Returns
+        -------
+        str
+            The letters of the given token.
+        """
+        return "".join(items)
+
+    def unsigned_int(self, items: Token) -> int:
+        """
+        Returns the unsigned integer of the given token.
+
+        Parameters
+        ----------
+        items : Token
+            The token to get the unsigned integer of.
+
+        Returns
+        -------
+        int
+            The unsigned integer of the given token.
+        """
+        return int("".join(items))
+
+    def int(self, items: Token) -> int:
+        """
+        Returns the integer of the given token.
+
+        Parameters
+        ----------
+        items : Token
+            The token to get the integer of.
+
+        Returns
+        -------
+        int
+            The integer of the given token.
+        """
+        return int("".join(items))
+
+
+class CrudeSelectionVisitor(Visitor):
+    """
+    A super class for visiting a Lark parse tree.
+
+    It is used to define the visit methods for the SelectionVisitor class.
+
+    Parameters
+    ----------
+    Visitor : Visitor
+        The type of the Visitor class to inherit from.
+    """
+
+    def atom(self, items: Tree) -> Atom:
+        """
+        Returns the atom of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the atom of.
+
+        Returns
+        -------
+        Atom
+            The atom of the given items.
+        """
+        return Atom(items.children[0], items.children[1])
+
+    def element(self, items: Tree) -> Element:
+        """
+        Returns the element of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the element of.
+
+        Returns
+        -------
+        Element
+            The element of the given items.
+        """
+        return Element(items.children[0])
+
+    def atom_type(self, items: Tree) -> str:
+        """
+        Returns the atom type of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the atom type of.
+
+        Returns
+        -------
+        str
+            The atom type of the given items.
+        """
+        return items.children[0]
+
+    def index(self, items: Tree) -> int:
+        """
+        Returns the index of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the index of.
+
+        Returns
+        -------
+        int
+            The index of the given items.
+        """
+        return items.children[0]
+
+    def indices(self, items: Tree) -> Np1DIntArray:
+        """
+        Returns the indices of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the indices of.
+
+        Returns
+        -------
+        Np1DIntArray
+            The indices of the given items.
+        """
+        if len(items.children) == 2:
+            return np.arange(items.children[0], items.children[1]+1)
+        elif len(items.children) == 3:
+            return np.arange(items.children[0], items.children[2]+1, items.children[1])
+
+    def residue(self, items: Tree) -> int | str:
+        """
+        Returns the residue of the given items.
+
+        Parameters
+        ----------
+        items : Tree
+            The items to get the residue of.
+
+        Returns
+        -------
+        int | str
+            The residue of the given items.
+        """
+        return items.children[0]
+
+    def visit(self, tree: Tree) -> None:
+        """
+        Visits the given tree.
+
+        Parameters
+        ----------
+        tree : Tree
+            The tree to visit.
+        """
+        super().visit(tree)
+
+
+class SelectionVisitor(CrudeSelectionVisitor):
     """
     A class for visiting a Lark parse tree and returning the indices of parsed selection.
 
@@ -245,7 +460,9 @@ class SelectionVisitor(Visitor):
         Np1DIntArray
             The indices of the atoms with the given atom type name.
         """
-        for index in _indices_by_atom_type_name(items[0], self.topology):
+        atomtype = super().atomtype(items)
+
+        for index in _indices_by_atom_type_name(atomtype, self.topology):
             self.selection.append(index)
 
         return np.array(self.selection)
@@ -265,14 +482,39 @@ class SelectionVisitor(Visitor):
             The indices of the given atom.
         """
 
+        atom = super().atom(items)
+
         if self.use_full_atom_info:
-            for index in _indices_by_atom(items[0], self.topology):
+            for index in _indices_by_atom(atom, self.topology):
                 self.selection.append(index)
         else:
-            for index in _indices_by_element_types(items[0], self.topology):
+            for index in _indices_by_element_types(atom, self.topology):
                 self.selection.append(index)
 
         return np.array(self.selection)
+
+    def element(self, items) -> Np1DIntArray:
+        """
+        Returns the indices of the given element type.
+
+        Parameters
+        ----------
+        items : 
+            The element type to get the indices of.
+
+        Returns
+        -------
+        Np1DIntArray
+            The indices of the given element type.
+        """
+
+        element = super().element(items)
+
+        for index in _indices_by_element_types(element, self.topology):
+            self.selection.append(index)
+
+    def residue(self, items) -> Np1DIntArray:
+        raise NotImplementedError("Residue selection is not implemented yet.")
 
     def index(self, items) -> Np1DIntArray:
         """
@@ -288,7 +530,9 @@ class SelectionVisitor(Visitor):
         Np1DIntArray
             The indices of the given atom.
         """
-        self.selection.append(items[0])
+        index = super().index(items)
+
+        self.selection.append(index)
 
         return np.array(self.selection)
 
@@ -306,12 +550,11 @@ class SelectionVisitor(Visitor):
         Np1DIntArray
             The indices of the given atoms.
         """
-        if len(items) == 2:
-            for index in range(items[0], items[1]):
-                self.selection.append(index)
-        elif len(items) == 3:
-            for index in range(items[0], items[2], items[1]):
-                self.selection.append(index)
+
+        indices = super().indices(items)
+
+        for index in indices:
+            self.selection.append(index)
 
         return np.array(self.selection)
 
@@ -381,14 +624,16 @@ def _indices_by_atom(atom: Atom, topology: Topology) -> Np1DIntArray:
     return indices
 
 
-def _indices_by_element_types(atom: Atom, topology: Topology) -> Np1DIntArray:
+def _indices_by_element_types(atom: Atom | Element, topology: Topology) -> Np1DIntArray:
     """
     Returns the indices of the given element type.
 
+    It can be use for both Atom and Element objects.
+
     Parameters
     ----------
-    atom: Atom
-        The atom type to get the indices of.
+    atom: Atom | Element
+        The atom or element to get the indices of.
     topology : Topology
         The topology to get the indices from.
 
@@ -397,8 +642,12 @@ def _indices_by_element_types(atom: Atom, topology: Topology) -> Np1DIntArray:
     Np1DIntArray
         The indices of the given element type.
     """
+
+    if isinstance(atom, Atom):
+        element = atom.element
+
     bool_indices = np.array(
-        [topology_atom.element == atom.element for topology_atom in topology.atoms])
+        [topology_atom.element == element for topology_atom in topology.atoms])
 
     indices = np.argwhere(bool_indices).flatten()
 
