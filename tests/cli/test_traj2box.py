0656bdda65bbc232b7c5214f8e75e0b027078417
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-02-14 12:56:56 +0100

    Fixed directory in test_traj2box

--- tests/cli/test_traj2box.py
+++ tests/cli/test_traj2box.py
@@ -10,13 +10,11 @@ from . import ArgparseNamespace
 
 @pytest.mark.parametrize("example_dir", ["traj2box"], indirect=False)
 def test_traj2box(test_with_data_dir):
-    traj2box(trajectory_files=[
-             "test.xyz"], vmd=False, output="test_box.dat")
+    traj2box(trajectory_files=["test.xyz"], vmd=False, output="test_box.dat")
 
     assert filecmp("box.dat", "test_box.dat")
 
-    traj2box(trajectory_files=[
-             "test.xyz"], vmd=True, output="test_box.vmd.xyz")
+    traj2box(trajectory_files=["test.xyz"], vmd=True, output="test_box.vmd.xyz")
 
     assert filecmp("box.vmd.xyz", "test_box.vmd.xyz")
 
@@ -28,16 +26,14 @@ def test_main(test_with_data_dir):
 
 
 @mock.patch('argparse.ArgumentParser.parse_args',
-            return_value=ArgparseNamespace(trajectory_file=["acof_triclinic.xyz",
-                                                            "acof_triclinic_2.xyz"], vmd=False, output="test_box.dat"))
+            return_value=ArgparseNamespace(trajectory_file=["test.xyz"], vmd=False, output="test_box.dat"))
 def main_box_file(mock_args):
     main()
     assert filecmp("box.dat", "test_box.dat")
 
 
 @mock.patch('argparse.ArgumentParser.parse_args',
-            return_value=ArgparseNamespace(trajectory_file=["acof_triclinic.xyz",
-                                                            "acof_triclinic_2.xyz"], vmd=True, output="test_box.vmd.xyz"))
+            return_value=ArgparseNamespace(trajectory_file=["test.xyz"], vmd=True, output="test_box.vmd.xyz"))
 def main_vmd(mock_args):
     main()
     assert filecmp("box.vmd.xyz", "test_box.vmd.xyz")
