e39893c69431e67a07e781d78840ae1abdf633c9
97gamjak <97gamjak@gmail.com>
2024-04-10 15:23:31 +0200

    add molecule tool started

--- PQAnalysis/io/api.py
+++ PQAnalysis/io/api.py
@@ -83,6 +83,41 @@ def write_trajectory(traj,
     writer.write(traj, type=type)
 
 
+def read_restart_file(filename: str,
+                      moldescriptor_filename: str | None = None,
+                      reference_residues: Residues | None = None,
+                      md_engine_format: MDEngineFormat | str = MDEngineFormat.PIMD_QMCF
+                      ) -> Frame:
+    """
+    API function for reading a restart file.
+
+    Parameters
+    ----------
+    filename : str
+        The filename of the restart file.
+    moldescriptor_filename : str | None, optional
+        The filename of the moldescriptor file that is read by the MoldescriptorReader to obtain the reference residues of the system, by default None
+    reference_residues : Residues | None, optional
+        The reference residues of the system, in general these are obtained by the MoldescriptorReader - only used if moldescriptor_filename is None, by default None
+    md_engine_format : MDEngineFormat | str, optional
+        The format of the restart file, by default MDEngineFormat.PIMD_QMCF
+
+    Returns
+    -------
+    Frame
+        The Frame object including the AtomicSystem and the Topology with the molecular types.
+    """
+
+    reader = RestartFileReader(
+        filename=filename,
+        moldescriptor_filename=moldescriptor_filename,
+        reference_residues=reference_residues,
+        md_engine_format=md_engine_format
+    )
+
+    return reader.read()
+
+
 def read_trajectory(filename: str,
                     md_format: MDEngineFormat | str = MDEngineFormat.PIMD_QMCF,
                     traj_format: TrajectoryFormat | str = TrajectoryFormat.XYZ,
