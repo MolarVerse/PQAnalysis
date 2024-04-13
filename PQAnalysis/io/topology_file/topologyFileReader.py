7932287c122e37cb5bbae2fa313e73e727f7f52e
Jakob Gamper <97gamjak@gmail.com>
2024-04-13 21:52:03 +0200

    implemented reading and writing of topology file in pq style

---
+++ PQAnalysis/io/topology_file/topologyFileReader.py
@@ -0,0 +1,104 @@
+from PQAnalysis.io import BaseReader
+from PQAnalysis.topology import Bond
+
+
+class TopologyFileReader(BaseReader):
+    def __init__(self, filename: str) -> None:
+        super().__init__(filename)
+
+    def read(self):
+        blocks = self.get_definitions()
+        self.parse_blocks(blocks)
+
+    def parse_blocks(self, blocks):
+        for key, value in blocks.items():
+            if key.lower() == "bonds":
+                self.parse_bonds(value)
+            elif key == "SHAKE":
+                self.parse_bonds(value)
+            elif key == "ANGLES":
+                self.parse_angles(value)
+            elif key == "DIHEDRALS":
+                self.parse_dihedrals(value)
+            elif key == "IMPROPERS":
+                self.parse_impropers(value)
+            else:
+                raise ValueError(f"Unknown block {key}")
+
+    def get_definitions(self):
+        with open(self.filename, "r") as file:
+            lines = file.readlines()
+
+            # remove all #.... parts from all lines
+            lines = [line.split("#")[0] for line in lines]
+
+            # remove all empty lines
+            lines = [line for line in lines if line.strip()]
+
+            # check if last line is END else raise error
+            if lines[-1].strip() != "END":
+                raise ValueError(
+                    "Something went wrong. Each block should end with 'END'")
+
+            # split all lines into blocks where each block ends with END
+            blocks = []
+            block = []
+            for line in lines:
+                if line.strip() == "END":
+                    blocks.append(block)
+                    block = []
+                else:
+                    block.append(line)
+
+            # make a dictionary for each block with the key being the first word of the first line of each block
+            # and the value being the rest of the block appart from the last line
+            data = {}
+            for block in blocks:
+                key = block[0].split()[0]
+                value = block[:-1]
+                data[key] = value
+
+            return data
+
+    def parse_bonds(self, block):
+        pass
+
+    def parse_angles(self, block):
+        pass
+
+    def parse_dihedrals(self, block):
+        pass
+
+    def parse_impropers(self, block):
+        pass
+
+    def parse_shake(self, block):
+        self._shake_bonds = []
+        for line in block:
+            if line.startswith("SHAKE"):
+                _, n_atoms, n_targets, n_link_shakes = line.split()
+                n_atoms = int(n_atoms)
+                n_targets = int(n_targets)
+                n_link_shakes = int(n_link_shakes)
+
+            else:
+                # if line contains a star at the fourth column, it is a link shake
+                if len(line.split()) == 4:
+                    index, target_index, distance, _ = line.split()
+                    is_linker = True
+                else:
+                    index, target_index, distance = line.split()
+                    is_linker = False
+
+                self._shake_bonds.append(
+                    Bond(
+                        int(index),
+                        int(target_index),
+                        float(distance),
+                        is_linker=is_linker
+                    )
+                )
+
+    @property
+    def shake_bonds(self):
+        return self._shake_bonds
--- PQAnalysis/topology/__init__.py
+++ PQAnalysis/topology/__init__.py
@@ -4,6 +4,9 @@ A package containing classes and functions to handle molecular/atomic topologies
 
 from .exceptions import TopologyError
 
+from .bond import Bond
 from .selection import Selection, SelectionCompatible
 from .topology import Topology
+
+# TODO: partially circular --- from .api import generate_shake_topology_file
 # TODO: partially circular --- from .shakeTopology import ShakeTopologyGenerator
