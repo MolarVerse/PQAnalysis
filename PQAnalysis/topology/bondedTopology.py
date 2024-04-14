c15d5fa3f8551618d2387de5b1d78c6b8901c8d3
Jakob Gamper <97gamjak@gmail.com>
2024-04-15 00:09:58 +0200

    topology file reader now fully implemented

--- PQAnalysis/topology/bondedTopology.py
+++ PQAnalysis/topology/bondedTopology.py
@@ -57,6 +57,74 @@ class BondedTopology:
                 self.shake_bonds.append(_bond)
 
     @property
+    def unique_bond1_indices(self):
+        return set([bond.index1 for bond in self.bonds])
+
+    @property
+    def unique_bond2_indices(self):
+        return set([bond.index2 for bond in self.bonds])
+
+    @property
+    def bond_linkers(self):
+        return [bond for bond in self.bonds if bond.is_linker]
+
+    @property
+    def unique_angle1_indices(self):
+        return set([angle.index1 for angle in self.angles])
+
+    @property
+    def unique_angle2_indices(self):
+        return set([angle.index2 for angle in self.angles])
+
+    @property
+    def unique_angle3_indices(self):
+        return set([angle.index3 for angle in self.angles])
+
+    @property
+    def angle_linkers(self):
+        return [angle for angle in self.angles if angle.is_linker]
+
+    @property
+    def unique_dihedral1_indices(self):
+        return set([dihedral.index1 for dihedral in self.dihedrals])
+
+    @property
+    def unique_dihedral2_indices(self):
+        return set([dihedral.index2 for dihedral in self.dihedrals])
+
+    @property
+    def unique_dihedral3_indices(self):
+        return set([dihedral.index3 for dihedral in self.dihedrals])
+
+    @property
+    def unique_dihedral4_indices(self):
+        return set([dihedral.index4 for dihedral in self.dihedrals])
+
+    @property
+    def dihedral_linkers(self):
+        return [dihedral for dihedral in self.dihedrals if dihedral.is_linker]
+
+    @property
+    def unique_improper1_indices(self):
+        return set([improper.index1 for improper in self.impropers])
+
+    @property
+    def unique_improper2_indices(self):
+        return set([improper.index2 for improper in self.impropers])
+
+    @property
+    def unique_improper3_indices(self):
+        return set([improper.index3 for improper in self.impropers])
+
+    @property
+    def unique_improper4_indices(self):
+        return set([improper.index4 for improper in self.impropers])
+
+    @property
+    def improper_linkers(self):
+        return [improper for improper in self.impropers if improper.is_linker]
+
+    @property
     def unique_shake_indices(self):
         return set([bond.index1 for bond in self.shake_bonds])
 
