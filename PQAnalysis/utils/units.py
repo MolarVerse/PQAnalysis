8add2de14d9077bf84631e7633e7ab0607f93db3
Jakob Gamper <97gamjak@gmail.com>
2024-04-20 00:25:37 +0200

    a lot of refactorin of NEP class

--- PQAnalysis/utils/units.py
+++ PQAnalysis/utils/units.py
@@ -6,5 +6,5 @@ kcal = Unum.unit('kcal', cal*1e3)
 
 mole = 6.02214076e23
 
-cal_per_mole = Unum.unit('cal/mol', J/4.184/mol)
+cal_per_mole = Unum.unit('cal/mol', J/4.184/mole)
 kcal_per_mole = Unum.unit('kcal/mol', cal_per_mole*1e3)
