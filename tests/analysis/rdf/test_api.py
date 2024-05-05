03193641834ed82fe9ec659f3eaef190b15dd7d0
Jakob Gamper <97gamjak@gmail.com>
2024-05-05 21:36:45 +0200

    added runtime type checking decorator

---
+++ tests/analysis/rdf/test_api.py
@@ -0,0 +1,18 @@
+import pytest
+
+from PQAnalysis.analysis.rdf.api import rdf
+
+from .. import pytestmark  # pylint: disable=unused-import
+from ...conftest import assert_logging
+
+
+# class TestRDFAPI:
+#     # def test_wrong_param_types(self, caplog):
+#     #     assert_logging(
+#     #         caplog=caplog,
+#     #         logging_name=rdf.__name__,
+#     #         logging_level="ERROR",
+#     #         message_to_test="Input file must be a string",
+#     #         function=rdf,
+#     #         input_file=1,
+#     #     )
