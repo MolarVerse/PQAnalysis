fab62683641b9388eb11efa7e999ae88f3bed0b0
97gamjak <97gamjak@gmail.com>
2024-05-06 10:19:51 +0200

    runtime type checking decorator implemented and tested for rdf api

--- tests/analysis/rdf/test_api.py
+++ tests/analysis/rdf/test_api.py
@@ -1,18 +1,22 @@
 import pytest
 
 from PQAnalysis.analysis.rdf.api import rdf
+from PQAnalysis.type_checking import _get_type_error_message
 
 from .. import pytestmark  # pylint: disable=unused-import
-from ...conftest import assert_logging
+from ...conftest import assert_logging_with_exception
 
 
-# class TestRDFAPI:
-#     # def test_wrong_param_types(self, caplog):
-#     #     assert_logging(
-#     #         caplog=caplog,
-#     #         logging_name=rdf.__name__,
-#     #         logging_level="ERROR",
-#     #         message_to_test="Input file must be a string",
-#     #         function=rdf,
-#     #         input_file=1,
-#     #     )
+class TestRDFAPI:
+    def test_wrong_param_types(self, caplog):
+        assert_logging_with_exception(
+            caplog=caplog,
+            logging_name="TypeChecking",
+            logging_level="ERROR",
+            message_to_test=_get_type_error_message(
+                "input_file", 1, str,
+            ),
+            exception=TypeError,
+            function=rdf,
+            input_file=1,
+        )
