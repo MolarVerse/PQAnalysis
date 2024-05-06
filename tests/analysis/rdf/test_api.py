c045e26ddfa50f14c05d1127e9cc935918bea155
97gamjak <97gamjak@gmail.com>
2024-05-06 09:36:53 +0200

    runtime_type_checking implemented

--- tests/analysis/rdf/test_api.py
+++ tests/analysis/rdf/test_api.py
@@ -6,13 +6,13 @@ from .. import pytestmark  # pylint: disable=unused-import
 from ...conftest import assert_logging
 
 
-# class TestRDFAPI:
-#     # def test_wrong_param_types(self, caplog):
-#     #     assert_logging(
-#     #         caplog=caplog,
-#     #         logging_name=rdf.__name__,
-#     #         logging_level="ERROR",
-#     #         message_to_test="Input file must be a string",
-#     #         function=rdf,
-#     #         input_file=1,
-#     #     )
+class TestRDFAPI:
+    def test_wrong_param_types(self, caplog):
+        assert_logging(
+            caplog=caplog,
+            logging_name=rdf.__name__,
+            logging_level="ERROR",
+            message_to_test="Input file must be a string",
+            function=rdf,
+            input_file=1,
+        )
