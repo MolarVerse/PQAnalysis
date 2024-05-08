6de6d9dba052f0fa960ee88af74481f6436fa560
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-05-08 15:44:50 +0200

    feat: Update Trajectory Reader class to include a window generator

--- PQAnalysis/io/traj_file/trajectory_reader.py
+++ PQAnalysis/io/traj_file/trajectory_reader.py
@@ -5,6 +5,7 @@ A module containing classes for reading a trajectory from a file.
 from __future__ import annotations
 
 # 3rd party modules
+import logging
 from beartype.typing import List, Generator
 from tqdm.auto import tqdm
 
@@ -15,6 +16,8 @@ from PQAnalysis.traj import Trajectory, TrajectoryFormat, MDEngineFormat
 from PQAnalysis.core import Cell
 from PQAnalysis.topology import Topology
 from PQAnalysis.io.base import BaseReader
+from PQAnalysis.utils.custom_logging import setup_logger
+from PQAnalysis import __package_name__
 
 # Local relative modules
 from .exceptions import TrajectoryReaderError
@@ -28,13 +31,17 @@ class TrajectoryReader(BaseReader):
     Inherited from BaseReader.
     """
 
-    def __init__(self,
-                 filename: str | List[str],
-                 traj_format: TrajectoryFormat | str = TrajectoryFormat.AUTO,
-                 md_format: MDEngineFormat | str = MDEngineFormat.PQ,
-                 topology: Topology | None = None,
-                 constant_topology: bool = True
-                 ) -> None:
+    # Set up the logger
+    logger = logging.getLogger(__package_name__).getChild(__qualname__)
+
+    def __init__(
+        self,
+        filename: str | List[str],
+        traj_format: TrajectoryFormat | str = TrajectoryFormat.AUTO,
+        md_format: MDEngineFormat | str = MDEngineFormat.PQ,
+        topology: Topology | None = None,
+        constant_topology: bool = True,
+    ) -> None:
         """
         Parameters
         ----------
@@ -52,6 +59,9 @@ class TrajectoryReader(BaseReader):
         """
         super().__init__(filename)
 
+        # Set up the logger
+        self.logger = setup_logger(self.logger)
+
         if not self.multiple_files:
             self.filenames = [self.filename]
 
@@ -65,6 +75,9 @@ class TrajectoryReader(BaseReader):
 
         self.md_format = MDEngineFormat(md_format)
         self.frame_reader = FrameReader(md_format=self.md_format)
+
+        # Calculate the number of frames in the trajectory
+        self.length_of_traj = self.calculate_number_of_frames()
 
         # NOTE: Progress bar is disabled by default
         #       This way the frame_generator can be used in other functions
@@ -80,11 +93,11 @@ class TrajectoryReader(BaseReader):
         same frame. The frame information is then read from the concatenated
         string with the FrameReader class and a Frame object is created.
 
-        In order to read the cell information given in the file, the cell 
+        In order to read the cell information given in the file, the cell
         information of the last frame is used for all following frames that
         do not have cell information.
 
-        If the trajectory is split into multiple files, the files are read one 
+        If the trajectory is split into multiple files, the files are read one
         after another and the frames are concatenated into a single trajectory.
 
         Parameters
@@ -107,39 +120,91 @@ class TrajectoryReader(BaseReader):
 
         return traj
 
-    def frame_generator(self) -> Generator[AtomicSystem]:
+    def frame_generator(
+        self,
+        trajectory_start: int = 0,
+        trajectory_stop: int | None = None,
+    ) -> Generator[AtomicSystem]:
         """
         A generator that yields the frames of the trajectory.
 
         The difference to the read method is that the read method returns the whole
-        trajectory at once, while the frame_generator yields the frames one after 
-        another. This is useful if the trajectory is very large and cannot be stored in memory.
+        trajectory at once, while the frame_generator yields the frames one after
+        another. This is useful if the trajectory is very large and cannot be stored
+        in memory.
 
-        This method is used to read the trajectory from the file. It reads the 
-        trajectory from the file and concatenates the lines of the same frame. 
+        This method is used to read the trajectory from the file. It reads the
+        trajectory from the file and concatenates the lines of the same frame.
         The frame information is then read from the concatenated string with the
         FrameReader class and a Frame object is created.
+
+        Parameters
+        ----------
+        trajectory_start : int, optional
+            The start index of the trajectory, by default 0.
+        trajectory_stop : int | None, optional
+            The last index of the trajectory, by default None, which then
+            set to the length of the trajectory.
+
+        Exceptions
+        ----------
+        IndexError
+            If trajectory_start is less than 0 or greater than the length
+            of the trajectory.
+            If trajectory_stop is less than 0 or greater than the length
+            of the trajectory.
 
         Yields
         ------
         Generator[AtomicSystem]
             The frames of the trajectory.
         """
+
+        if trajectory_stop is None:
+            trajectory_stop = self.length_of_traj
+
+        # If trajectory_start is greater than or equal to trajectory_stop,
+        # raise an IndexError
+        if trajectory_start >= trajectory_stop:
+            self.logger.error(
+                "start index is greater than or equal to the stop index",
+                exception=IndexError,
+            )
+
+        # If trajectory_start is less than 0 or greater than the
+        # length of the trajectory, raise an IndexError
+        if trajectory_start < 0 or trajectory_start > self.length_of_traj:
+            self.logger.error(
+                "start index is less than 0 or greater than the length of the trajectory",
+                exception=IndexError,
+            )
+
+        # If trajectory_stop is less than 0 or greater than the
+        # length of the trajectory, raise an IndexError
+        if trajectory_stop < 0 or trajectory_stop > self.length_of_traj:
+            self.logger.error(
+                "stop index is less than 0 or greater than the length of the trajectory",
+                exception=IndexError,
+            )
+
         last_cell = None
         for filename in self.filenames:
-            with open(filename, 'r', encoding='utf-8') as self.file:
+            with open(filename, "r", encoding="utf-8") as self.file:
                 sum_lines = sum(1 for _ in self.file)
 
-            with open(filename, 'r', encoding='utf-8') as self.file:
+            with open(filename, "r", encoding="utf-8") as self.file:
                 frame_lines = []
-                for line in tqdm(self.file, total=sum_lines, disable=not self.with_progress_bar):
+                for line in tqdm(
+                    self.file, total=sum_lines, disable=not self.with_progress_bar
+                ):
                     stripped_line = line.strip()
-                    if stripped_line == '' or not stripped_line[0].isdigit():
+                    if stripped_line == "" or not stripped_line[0].isdigit():
                         frame_lines.append(line)
                     else:
                         if frame_lines:
                             frame = self._read_single_frame(
-                                ''.join(frame_lines), self.topology)
+                                "".join(frame_lines), self.topology
+                            )
                             if frame.cell.is_vacuum and last_cell is not None:
                                 frame.cell = last_cell
                             last_cell = frame.cell
@@ -149,8 +214,7 @@ class TrajectoryReader(BaseReader):
                         frame_lines = [line]
 
                 if frame_lines:
-                    frame = self._read_single_frame(
-                        ''.join(frame_lines), self.topology)
+                    frame = self._read_single_frame("".join(frame_lines), self.topology)
                     if frame.cell.is_vacuum and last_cell is not None:
                         frame.cell = last_cell
                     last_cell = frame.cell
@@ -158,8 +222,110 @@ class TrajectoryReader(BaseReader):
                 if self.constant_topology and self.topology is not None:
                     self.topology = frame.topology
 
-    def window_generator(self, window_size: int) -> Generator[List[AtomicSystem]]:
-        pass
+    def window_generator(
+        self,
+        window_size: int,
+        window_gap: int = 1,
+        trajectory_start: int = 0,
+        trajectory_stop: int | None = None,
+    ) -> Generator[Trajectory]:
+        """
+        A generator that yields the windows of the trajectory using the specified
+        window size and gap. The windows are generated by sliding a window of the
+        specified size over the trajectory with the specified gap. It uses the
+        frame_generator method to read the frames of the trajectory.
+
+        Parameters
+        ----------
+        window_size : int
+            The size of the window.
+        window_gap : int, optional
+            The gap size between two windows, by default 1
+        trajectory_start : int, optional
+            The start index of the first window, by default 0
+        trajectory_stop : int | None, optional
+            The last index of the last window, by default None, which then
+            set to the length of the trajectory.
+
+        Raises
+        ------
+        IndexError
+            If window_size is less than 1 or greater than the length of
+            the trajectory.
+            If window_gap is less than 1 or greater than the length of
+            the trajectory.
+            If window_size is greater than trajectory_stop - trajectory_start.
+
+        Warning
+        -------
+        If not all frames are included in the windows, a warning is issued.
+
+        Yields
+        ------
+        Generator[Trajectory]
+            An generator over the windows of the trajectory with the specified
+            window size and gap.
+        """
+
+        # If trajectory_stop is not provided, set it to the length of the trajectory
+        if trajectory_stop is None:
+            trajectory_stop = self.length_of_traj
+
+        # If window_step is less than 1 or greater than
+        # the length of the trajectory, raise an IndexError
+        if window_size < 1 or window_size > self.length_of_traj:
+            self.logger.error(
+                "window size can not be less than 1 or greater than the length of the trajectory",
+                exception=IndexError,
+            )
+
+        # If window_gap is less than 1 or greater than
+        # the length of the trajectory, raise an IndexError
+        if window_gap < 1 or window_gap > self.length_of_traj:
+            self.logger.error(
+                "window gap can not be less than 1 or greater than the length of the trajectory",
+                exception=IndexError,
+            )
+
+        # If window_size is greater than trajectory_stop - trajectory_start, raise an IndexError
+        if window_size > trajectory_stop - trajectory_start:
+            self.logger.error(
+                "window size is greater than the trajectory_stop - trajectory_start",
+                exception=IndexError,
+            )
+
+        # Check if all frames are included in the windows
+        # Length of the trajectory - window_size should be divisible by window_gap
+        if ((trajectory_stop - trajectory_start) - window_size) % window_gap != 0:
+            self.logger.warning(
+                "Not all frames are included in the windows. Check the window size and gap."
+            )
+
+        generator = self.frame_generator()
+
+        # Skip the frames up to trajectory_start
+        for _ in range(trajectory_start):
+            next(generator)
+
+        # reads first window and converts it to a queue
+        window = Trajectory([generator.__next__() for _ in range(window_size)])
+
+        # yield the first window
+        yield window
+
+        # generate the rest of the windows up to trajectory_stop
+        for _ in range(
+            trajectory_start + window_gap, trajectory_stop - window_size + 1, window_gap
+        ):
+
+            # pop the first frame and append the next frame for window_gap times to
+            # get the next window
+            for _ in range(window_gap):
+                window.frames.pop(0)
+                window.append(next(generator))
+
+            # yield the next window
+            yield window
 
     def calculate_number_of_frames(self) -> int:
         """
@@ -174,9 +340,11 @@ class TrajectoryReader(BaseReader):
         n_frames = 0
 
         for filename in self.filenames:
-            with open(filename, 'r', encoding='utf-8') as f:
+            with open(filename, "r", encoding="utf-8") as f:
                 lines = f.readlines()
-                n_frames += int(len(lines) / int(lines[0].split()[0]))
+                
+                # +2 for the cell and atom count lines
+                n_frames += int(len(lines) / int(lines[0].split()[0]) + 2)
 
         return n_frames
 
@@ -196,10 +364,10 @@ class TrajectoryReader(BaseReader):
         """
         A generator that yields the cells of the trajectory.
 
-        This method is used to read the cells from the file. 
+        This method is used to read the cells from the file.
         It reads the cells from the file and yields them one after
-        another. If the cell information is not given in the 
-        file, the cell information of the last frame is used 
+        another. If the cell information is not given in the
+        file, the cell information of the last frame is used
         for all following frames that do not have cell information.
 
         Yields
@@ -208,12 +376,12 @@ class TrajectoryReader(BaseReader):
             The list of cells read from the trajectory.
         """
         last_cell = None
-        with open(self.filenames[0], 'r', encoding='utf-8') as f:
+        with open(self.filenames[0], "r", encoding="utf-8") as f:
             line = f.readline()
             n_atoms = int(line.split()[0])
 
         for filename in self.filenames:
-            with open(filename, 'r', encoding='utf-8') as f:
+            with open(filename, "r", encoding="utf-8") as f:
                 for line in f:
                     stripped_line = line.strip()
                     splitted_line = stripped_line.split()
@@ -231,7 +399,7 @@ class TrajectoryReader(BaseReader):
                         cell = Cell(
                             float(splitted_line[1]),
                             float(splitted_line[2]),
-                            float(splitted_line[3])
+                            float(splitted_line[3]),
                         )
 
                         yield cell
@@ -244,7 +412,7 @@ class TrajectoryReader(BaseReader):
                             float(splitted_line[3]),
                             float(splitted_line[4]),
                             float(splitted_line[5]),
-                            float(splitted_line[6])
+                            float(splitted_line[6]),
                         )
 
                         yield cell
@@ -259,13 +427,12 @@ class TrajectoryReader(BaseReader):
 
                     last_cell = cell
 
-                    for _ in range(n_atoms+1):
+                    for _ in range(n_atoms + 1):
                         next(f, None)  # Skip the next n_atoms+1 lines
 
-    def _read_single_frame(self,
-                           frame_string: str,
-                           topology: Topology | None = None
-                           ) -> AtomicSystem:
+    def _read_single_frame(
+        self, frame_string: str, topology: Topology | None = None
+    ) -> AtomicSystem:
         """
         Reads a single frame from the given string.
 
@@ -287,4 +454,5 @@ class TrajectoryReader(BaseReader):
             If the first atom in the frame is not X for QMCFC.
         """
         return self.frame_reader.read(
-            frame_string, traj_format=self.traj_format, topology=topology)
+            frame_string, traj_format=self.traj_format, topology=topology
+        )
