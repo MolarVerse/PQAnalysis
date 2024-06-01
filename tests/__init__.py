bc9e694dc6e48dd31fb9cf1973354356904c53ae
Jakob Gamper <97gamjak@gmail.com>
2024-06-02 00:05:39 +0200

    tests: first integration tests of add_molecules added

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,7 @@ Unit tests for the PQAnalysis package.
 
 import os
 
+__beartype_level__ = os.environ.get("PQANALYSIS_BEARTYPE_LEVEL", "RELEASE")
+__beartype_level__ = __beartype_level__.upper()
+
 os.environ['CONSTANT_SEED'] = '42'
