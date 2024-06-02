77583b0073e5ff3e8f0320bdca934f3cde80a625
Jakob Gamper <97gamjak@gmail.com>
2024-06-02 15:40:59 +0200

    tests: added some unit tests for TopologyFileWriter

--- PQAnalysis/io/topology_file/__init__.py
+++ PQAnalysis/io/topology_file/__init__.py
@@ -2,5 +2,7 @@
 A subpackage to handle trajectory files.
 """
 
+from .exceptions import TopologyFileError
+
 from .topology_file_reader import TopologyFileReader
 from .topology_file_writer import TopologyFileWriter
