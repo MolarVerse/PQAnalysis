9f13b0d2f659734ff433eae483ccde097f90b755
Jakob Gamper <97gamjak@gmail.com>
2024-06-11 23:29:41 +0200

    $ decision for cython not .c - performance seems to be the same

--- PQAnalysis/io/traj_file/mytest.pyx
+++ PQAnalysis/io/traj_file/mytest.pyx
@@ -1,9 +1,8 @@
-# mytest.pyx
 import numpy as np
 cimport numpy as np
 from libc.stdio cimport sscanf
 
-def process_lines(input, int n_atoms):
+def process_lines_with_atoms(input, int n_atoms):
     cdef np.ndarray[np.float32_t, ndim=2] xyz = np.zeros((n_atoms, 3), dtype=np.float32)
     cdef list atoms = [None] * n_atoms
     cdef char[5] atom
@@ -25,3 +24,22 @@ def process_lines(input, int n_atoms):
         atoms[i] = atom.decode('utf-8')
 
     return atoms, xyz
+
+def process_lines(input, int n_atoms):
+    cdef np.ndarray[np.float32_t, ndim=2] xyz = np.zeros((n_atoms, 3), dtype=np.float32)
+    cdef float x, y, z
+    cdef int ret
+
+    for i in range(n_atoms):
+        line = input[i]
+        line_str = line.encode('utf-8')
+
+        ret = sscanf(line_str, "%*s %f %f %f", &x, &y, &z)
+        if ret != 3:
+            raise ValueError("Could not parse line")
+
+        xyz[i, 0] = x
+        xyz[i, 1] = y
+        xyz[i, 2] = z
+
+    return xyz
