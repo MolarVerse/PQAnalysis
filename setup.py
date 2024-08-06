7048666713c287af16772ea2b1c1c22b041241c0
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-08-06 09:06:01 +0200

    chore: Update setup.py, compiler directive for Python 3 compatibility is not needed

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ module = cythonize(
         'PQAnalysis.io.traj_file.process_lines',
         sources=['PQAnalysis/io/traj_file/process_lines.pyx'],
         include_dirs=[np.get_include()]
-    ),
-    compiler_directives={'language_level': 3}
+    )
 )
 
 setup(
