b2508334564aae2e867c02b1c5ae4b2daec54128
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2025-02-06 11:35:11 +0100

    fix: update restart writer to conditionally include velocities and forces in output

--- PQAnalysis/io/restart_file/restart_writer.py
+++ PQAnalysis/io/restart_file/restart_writer.py
@@ -20,7 +20,6 @@ from PQAnalysis.type_checking import runtime_type_checking
 from .exceptions import RestartFileWriterError
 
 
-
 class RestartFileWriter(BaseWriter):
 
     """
@@ -192,9 +191,9 @@ class RestartFileWriter(BaseWriter):
             if len(atom_counter) != frame.n_atoms:
                 cls.logger.error(
                     (
-                    "The atom counter has to have the same length as "
-                    "the number of atoms in the frame if it is given "
-                    "as an array."
+                        "The atom counter has to have the same length as "
+                        "the number of atoms in the frame if it is given "
+                        "as an array."
                     ),
                     exception=RestartFileWriterError
                 )
@@ -219,9 +218,11 @@ class RestartFileWriter(BaseWriter):
             line = ""
 
             line += f"{atom.name}    {atom_counter[i]}    {residue}    "
-            line += f"{pos[0]} {pos[1]} {pos[2]} "
-            line += f"{vel[0]} {vel[1]} {vel[2]} "
-            line += f"{force[0]} {force[1]} {force[2]}"
+            line += f"{pos[0]} {pos[1]} {pos[2]}"
+
+            if (frame.has_vel and frame.has_forces) or md_engine_format != MDEngineFormat.PQ:
+                line += f" {vel[0]} {vel[1]} {vel[2]}"
+                line += f" {force[0]} {force[1]} {force[2]}"
 
             if md_engine_format != MDEngineFormat.PQ:
                 line += f" {pos[0]} {pos[1]} {pos[2]} "
