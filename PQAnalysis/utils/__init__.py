b770b02a350b1ba1e5d075d1c5120b57df5237ea
Josef M. Gallmetzer <64498081+galjos@users.noreply.github.com>
2026-06-08 23:29:52 +0200

    feat: add simulation time utility (#135)

--- PQAnalysis/utils/__init__.py
+++ PQAnalysis/utils/__init__.py
@@ -4,3 +4,4 @@ A package containing classes and functions with common use.
 
 from .common import print_header, __header__
 from .decorators import count_decorator, instance_function_count_decorator, timeit_in_class
+from .units import calculate_simulation_time
